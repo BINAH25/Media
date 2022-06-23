b5ab0785378ed625b9add83c208979b97bfdb186
BINAH LOUIS <visteen192@gmail.com>
2022-06-23 23:30:58 +0000

    make it better

--- src/api/UploadImageRequest.js
+++
@@ -1,4 +0,0 @@
-import axios from "axios";
-const API = axios.create({ baseURL: "http://localhost:5000" });
-export const UploadImage = (data) => API.post("/upload/", data);
-export const UploadPost = (data) => API.post("/post", data);
--- src/reducer/index.js
+++
@@ -1,5 +0,0 @@
-import { combineReducers } from "redux";
-
-import AuthReducer from "./AuthReducer";
-import PostReducer from "./PostReducer";
-export const reducers = combineReducers({ AuthReducer, PostReducer });
